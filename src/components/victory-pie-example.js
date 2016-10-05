d4a76fd67f4b2610c2e272ed1384729ba336b4d1
Angela Nicholas <angelaknicholas@gmail.com>
2016-10-04 21:25:41 -0700

    add more controls to pie example

--- src/components/victory-pie-example.js
+++ src/components/victory-pie-example.js
@@ -8,23 +8,41 @@ import { defaultPropMap } from "../utils/props";
 import { colorScales } from "../utils/colors";
 import { styles } from "../utils/styles";
 
+const endAngles = ["-180", "-135", "-90"];
+const startAngles = ["90", "135", "180"];
+
 export default class VictoryPieExample extends Component {
   static displayName = "VictoryPieExample";
 
   constructor(props) {
     super(props);
+
     this.handleColorChange = this.handleColorChange.bind(this);
     this.handleDatasetChange = this.handleDatasetChange.bind(this);
     this.handleInnerRadiusChange = this.handleInnerRadiusChange.bind(this);
+    this.handleEndAngleChange = this.handleEndAngleChange.bind(this);
+    this.handlePadAngleChange = this.handlePadAngleChange.bind(this);
+    this.handleStartAngleChange = this.handleStartAngleChange.bind(this);
+
     this.state = {
       innerRadius: 0,
+      padAngle: 0,
       selectedColorIndex: 0,
       selectedDatasetIndex: 0,
+      selectedEndAngleIndex: 0,
+      selectedStartAngleIndex: 2,
     };
   }
 
   render() {
-    const { innerRadius, selectedColorIndex, selectedDatasetIndex } = this.state;
+    const {
+      innerRadius,
+      padAngle,
+      selectedColorIndex,
+      selectedDatasetIndex,
+      selectedEndAngleIndex,
+      selectedStartAngleIndex,
+    } = this.state;
     const props = defaultPropMap.VictoryPie;
     const { data, ...other } = props;
 
@@ -37,6 +55,9 @@ export default class VictoryPieExample extends Component {
             colorScale={colorScales[selectedColorIndex]}
             data={data[selectedDatasetIndex]}
             innerRadius={innerRadius}
+            startAngle={parseInt(startAngles[selectedStartAngleIndex])}
+            endAngle={parseInt(endAngles[selectedEndAngleIndex])}
+            padAngle={padAngle}
           />
         </View>
         <ChartControls>
@@ -55,26 +76,50 @@ export default class VictoryPieExample extends Component {
             onChange={this.handleInnerRadiusChange}
             title="innerRadius"
           />
+          <ToggleControl
+            onChange={this.handleStartAngleChange}
+            selectedIndex={selectedStartAngleIndex}
+            title="startAngle"
+            values={startAngles}
+          />
+          <ToggleControl
+            onChange={this.handleEndAngleChange}
+            selectedIndex={selectedEndAngleIndex}
+            title="endAngle"
+            values={endAngles}
+          />
+          <SliderControl
+            min={0}
+            max={10}
+            onChange={this.handlePadAngleChange}
+            title="padAngle"
+          />
         </ChartControls>
       </View>
     );
   }
 
   handleColorChange(ev) {
-    this.setState({
-      selectedColorIndex: ev.nativeEvent.selectedSegmentIndex,
-    });
+    this.setState({ selectedColorIndex: ev.nativeEvent.selectedSegmentIndex });
   }
 
   handleDatasetChange(ev) {
-    this.setState({
-      selectedDatasetIndex: ev.nativeEvent.selectedSegmentIndex,
-    });
+    this.setState({ selectedDatasetIndex: ev.nativeEvent.selectedSegmentIndex });
   }
 
-  handleInnerRadiusChange(value) {
-    this.setState({
-      sliderValue: value,
-    });
+  handleEndAngleChange(ev) {
+    this.setState({ selectedEndAngleIndex: ev.nativeEvent.selectedSegmentIndex });
+  }
+
+  handleInnerRadiusChange(innerRadius) {
+    this.setState({ innerRadius });
+  }
+
+  handlePadAngleChange(padAngle) {
+    this.setState({ padAngle });
+  }
+
+  handleStartAngleChange(ev) {
+    this.setState({ selectedStartAngleIndex: ev.nativeEvent.selectedSegmentIndex });
   }
 }

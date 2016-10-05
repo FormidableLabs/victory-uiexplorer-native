6505108c8567a37a0d8fbfb8fe7919fa482f4f2e
Angela Nicholas <angelaknicholas@gmail.com>
2016-10-04 21:25:06 -0700

    add min and max props to sliderControl

--- src/components/slider-control.js
+++ src/components/slider-control.js
@@ -3,6 +3,8 @@ import { Slider, View } from "react-native";
 import Title from "../components/title";
 
 const SliderControl = ({
+  max = 100,
+  min = 0,
   onChange = () => {},
   title,
   value = 0,
@@ -10,8 +12,8 @@ const SliderControl = ({
   <View style={{ marginBottom: 20 }}>
     <Title text={title} style={{ marginBottom: 10 }} />
     <Slider
-      minimumValue={0}
-      maximumValue={100}
+      minimumValue={min}
+      maximumValue={max}
       onValueChange={onChange}
       step={1}
       value={value}
@@ -20,6 +22,8 @@ const SliderControl = ({
 );
 
 SliderControl.propTypes = {
+  max: PropTypes.number,
+  min: PropTypes.number,
   onChange: PropTypes.func,
   title: PropTypes.string,
   value: PropTypes.number,

b44bd4306651de9e4515632fd628789565e07934
Angela Nicholas <angelaknicholas@gmail.com>
2016-10-05 18:08:25 -0700

    add bar chart orientation and barWidth controls

--- src/components/victory-bar-example.js
+++ src/components/victory-bar-example.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 import { VictoryBar } from "victory-native";
+import { VictoryTransition } from "victory-core";
 import ChartControls from "../components/chart-controls";
-import DataToggle from "../components/toggle-control";
+import ToggleControl from "../components/toggle-control";
+import SliderControl from "../components/slider-control";
 import { defaultPropMap } from "../utils/props";
 import { styles } from "../utils/styles";
+
+const orientations = ["vertical", "horizontal"];
+const horizontalChartPadding = { top: 50, right: 80, bottom: 50, left: 80 };
 
 export default class VictoryBarExample extends Component {
   static displayName = "VictoryBarExample";
@@ -12,29 +17,54 @@ export default class VictoryBarExample extends Component {
   constructor(props) {
     super(props);
     this.handleDatasetChange = this.handleDatasetChange.bind(this);
+    this.handleOrientationChange = this.handleOrientationChange.bind(this);
+    this.handleBarWidthChange = this.handleBarWidthChange.bind(this);
     this.state = {
+      barWidth: 40,
       selectedDatasetIndex: 0,
+      selectedOrientationIndex: 0,
     };
   }
 
   render() {
-    const { selectedDatasetIndex } = this.state;
-    const props = defaultPropMap.VictoryBar;
-    const { data, ...other } = props;
+    const { barWidth, selectedDatasetIndex, selectedOrientationIndex } = this.state;
+    const defaultProps = defaultPropMap.VictoryBar;
+    const { data, ...otherDefaultProps } = defaultProps;
+    const isHorizontal = orientations[selectedOrientationIndex] === "horizontal";
+    delete otherDefaultProps.style;
 
     return (
       <View style={styles.container}>
         <View style={styles.chartWrapper}>
-          <VictoryBar
-            animate={{ duration: 400 }}
-            data={data[selectedDatasetIndex]}
-            {...other}
-          />
+          <VictoryTransition animationWhitelist={["data"]}>
+            <VictoryBar
+              {...otherDefaultProps}
+              animate={{ duration: 400 }}
+              data={data[selectedDatasetIndex]}
+              horizontal={isHorizontal}
+              padding={isHorizontal ? horizontalChartPadding : 30}
+              style={{ data: { width: barWidth } }}
+            />
+          </VictoryTransition>
         </View>
         <ChartControls>
-          <DataToggle
+          <ToggleControl
             onChange={this.handleDatasetChange}
             selectedIndex={selectedDatasetIndex}
+            title="data"
+          />
+          <ToggleControl
+            onChange={this.handleOrientationChange}
+            selectedIndex={selectedOrientationIndex}
+            title="orientation"
+            values={orientations}
+          />
+          <SliderControl
+            min={5}
+            max={60}
+            onChange={this.handleBarWidthChange}
+            title="barWidth"
+            value={barWidth}
           />
         </ChartControls>
       </View>
@@ -42,8 +72,14 @@ export default class VictoryBarExample extends Component {
   }
 
   handleDatasetChange(ev) {
-    this.setState({
-      selectedDatasetIndex: ev.nativeEvent.selectedSegmentIndex,
-    });
+    this.setState({ selectedDatasetIndex: ev.nativeEvent.selectedSegmentIndex });
+  }
+
+  handleOrientationChange(ev) {
+    this.setState({ selectedOrientationIndex: ev.nativeEvent.selectedSegmentIndex });
+  }
+
+  handleBarWidthChange(barWidth) {
+    this.setState({ barWidth });
   }
 }

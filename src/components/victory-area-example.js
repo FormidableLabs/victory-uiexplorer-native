260e8d2e5b2b74915d649de4fb724ba109ccb243
Angela Nicholas <angelaknicholas@gmail.com>
2016-10-06 20:21:42 -0700

    add area chart example controls

--- src/components/victory-area-example.js
+++ src/components/victory-area-example.js
@@ -1,42 +1,96 @@
 import React, { Component } from "react";
 import { View } from "react-native";
-import { VictoryArea } from "victory-native";
+import { VictoryArea, VictoryGroup, VictoryScatter } from "victory-native";
 import ChartControls from "../components/chart-controls";
-import DataToggle from "../components/toggle-control";
-import VictoryAreaChartWrapper from "../components/victory-area-chart-wrapper";
+import ToggleControl from "../components/toggle-control";
+import Checkbox from "../components/checkbox";
 import { defaultPropMap } from "../utils/props";
 import { styles } from "../utils/styles";
+import { colorScale, colorScales } from "../utils/colors";
+
+const brights = colorScales[1];
+const xDomain = { x: [1.25, 4.75] };
+const fills = [colorScale[2], brights[1], brights[2], brights[3]];
 
 export default class VictoryAreaExample extends Component {
   static displayName = "VictoryAreaExample";
 
   constructor(props) {
     super(props);
+    this.handleDataMarkerChange = this.handleDataMarkerChange.bind(this);
     this.handleDatasetChange = this.handleDatasetChange.bind(this);
+    this.handleFillChange = this.handleFillChange.bind(this);
     this.state = {
       selectedDatasetIndex: 0,
+      selectedFillIndex: 0,
+      showDataMarkers: false,
     };
   }
 
   render() {
-    const { selectedDatasetIndex } = this.state;
-    const props = defaultPropMap.VictoryArea;
-    const { data, ...other } = props;
+    const {
+      selectedDatasetIndex,
+      selectedFillIndex,
+      showDataMarkers,
+    } = this.state;
+    const defaultProps = defaultPropMap.VictoryArea;
+    const { data, ...otherDefaultProps } = defaultProps;
+    const selectedDataset = data[selectedDatasetIndex];
+    const selectedFill = fills[selectedFillIndex];
 
     return (
       <View style={styles.container}>
-        <VictoryAreaChartWrapper>
-          <VictoryArea
-            animate={{ duration: 400 }}
-            data={data[selectedDatasetIndex]}
-            standalone={false}
-            {...other}
-          />
-        </VictoryAreaChartWrapper>
+        <View style={styles.chartWrapper}>
+          <VictoryGroup animate={{ duration: 400 }}>
+            <VictoryArea
+              {...otherDefaultProps}
+              data={selectedDataset}
+              domain={xDomain}
+              style={{
+                data: {
+                  fill: selectedFill,
+                  opacity: 0.5,
+                  stroke: "transparent",
+                },
+              }}
+            />
+            {showDataMarkers &&
+              <VictoryScatter
+                {...otherDefaultProps}
+                data={selectedDataset}
+                domain={xDomain}
+                labels={["a", "b", "c", "d", "e"]}
+                size={4}
+                style={{
+                  data: {
+                    fill: selectedFill,
+                  },
+                  labels: {
+                    fill: selectedFill,
+                    fontSize: 12,
+                    fontWeight: "600",
+                    padding: 12,
+                  },
+                }}
+              />
+            }
+          </VictoryGroup>
+        </View>
         <ChartControls>
-          <DataToggle
+          <ToggleControl
             onChange={this.handleDatasetChange}
             selectedIndex={selectedDatasetIndex}
+            title="data"
+          />
+          <ToggleControl
+            onChange={this.handleFillChange}
+            selectedIndex={selectedFillIndex}
+            title="fill"
+            values={["Blue Gray", "Purple", "Pink", "Persimmon"]}
+          />
+          <Checkbox
+            label="Show data markers and labels"
+            onChange={this.handleDataMarkerChange}
           />
         </ChartControls>
       </View>
@@ -46,6 +100,16 @@ export default class VictoryAreaExample extends Component {
   handleDatasetChange(ev) {
     this.setState({
       selectedDatasetIndex: ev.nativeEvent.selectedSegmentIndex,
+    });
+  }
+
+  handleDataMarkerChange() {
+    this.setState({ showDataMarkers: !this.state.showDataMarkers });
+  }
+
+  handleFillChange(ev) {
+    this.setState({
+      selectedFillIndex: ev.nativeEvent.selectedSegmentIndex,
     });
   }
 }

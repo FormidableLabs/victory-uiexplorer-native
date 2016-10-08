be0d3712e0b802eabfd6d8e32d67348769ba5d65
Angela Nicholas <angelaknicholas@gmail.com>
2016-10-07 21:37:39 -0700

    add candlestick chart example controls

--- src/components/victory-candlestick-example.js
+++ src/components/victory-candlestick-example.js
@@ -5,26 +5,40 @@ import { VictoryCandlestick } from "victory-native";
 import ChartControls from "./chart-controls";
 import ChartWrapper from "./chart-wrapper";
 import Toggle from "./toggle";
+import Checkbox from "./checkbox";
 
+import { colorScales, colorScaleToggleValues } from "../utils/colors";
 import { defaultPropMap } from "../utils/props";
 import { styles } from "../utils/styles";
 
 const defaultProps = defaultPropMap.VictoryCandlestick;
-const { data, ...otherDefaultProps } = defaultProps;
+const { data, style: defaultStyles, ...otherDefaultProps } = defaultProps;
 
 export default class VictoryCandlestickExample extends Component {
   static displayName = "VictoryCandlestickExample";
 
   constructor(props) {
     super(props);
+
+    this.handleColorScaleChange = this.handleColorScaleChange.bind(this);
     this.handleDatasetChange = this.handleDatasetChange.bind(this);
+    this.handleLabelsChange = this.handleLabelsChange.bind(this);
+
     this.state = {
+      selectedColorScaleIndex: 0,
       selectedDatasetIndex: 0,
+      showLabels: false,
     };
   }
 
   render() {
-    const { selectedDatasetIndex } = this.state;
+    const {
+      selectedColorScaleIndex,
+      selectedDatasetIndex,
+      showLabels,
+    } = this.state;
+
+    const selectedColorScale = colorScales[selectedColorScaleIndex];
 
     return (
       <View style={styles.container}>
@@ -36,6 +50,18 @@ export default class VictoryCandlestickExample extends Component {
               onLoad: { duration: 0.0000001 },
             }}
             data={data[selectedDatasetIndex]}
+            labels={showLabels ? ["a", "b", "c", "d", "e"] : undefined}
+            style={selectedColorScaleIndex === 0 ? defaultStyles : {
+              data: {
+                fill: selectedColorScale[2],
+                stroke: selectedColorScale[1],
+              },
+              labels: {
+                fill: selectedColorScale[1],
+                fontSize: 14,
+                padding: 6,
+              },
+            }}
           />
         </ChartWrapper>
         <ChartControls>
@@ -44,12 +70,30 @@ export default class VictoryCandlestickExample extends Component {
             selectedIndex={selectedDatasetIndex}
             title="data"
           />
+          <Toggle
+            onChange={this.handleColorScaleChange}
+            selectedIndex={selectedColorScaleIndex}
+            title="colorScale"
+            values={colorScaleToggleValues}
+          />
+          <Checkbox
+            label="Show labels"
+            onChange={this.handleLabelsChange}
+          />
         </ChartControls>
       </View>
     );
   }
 
+  handleColorScaleChange(ev) {
+    this.setState({ selectedColorScaleIndex: ev.nativeEvent.selectedSegmentIndex });
+  }
+
   handleDatasetChange(ev) {
     this.setState({ selectedDatasetIndex: ev.nativeEvent.selectedSegmentIndex });
+  }
+
+  handleLabelsChange() {
+    this.setState({ showLabels: !this.state.showLabels });
   }
 }
